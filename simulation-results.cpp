98ac86fa74e86d919f26d97f9620776910459f3e
Pratik Kumar <pratik.kumar.ece12@iitbhu.ac.in>
2015-11-10 21:30:53 +0530

    Gaussian Random Noise, output formatting

---
+++ simulation-results.cpp
@@ -0,0 +1,3 @@
+![simulation](https://cloud.githubusercontent.com/assets/7613257/11067366/2618423c-87f2-11e5-8173-8efb0846cbed.jpg)
+
+![viterbisim](https://cloud.githubusercontent.com/assets/7613257/11067388/44f1c2b4-87f2-11e5-859e-1d4c45a96f3e.jpg)
